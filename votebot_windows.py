85112e636fd0b519604192124b27fa4f13ab9858
Ebnater <62059445+Ebnater@users.noreply.github.com>
2021-01-04 12:30:11 +0100

    New Version
    New config file, Script for Linux and Script for Windows, added uBlock origin for faster bot

---
+++ votebot_windows.py
@@ -0,0 +1,73 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+import yaml
+import time
+import os
+
+with open('config.yaml') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+
+        user = data['user']
+        server = data['server']
+        headless = data['headless']
+        executable_dir = data['path']
+
+CURR_DIR = os.path.dirname(os.path.realpath(__file__))
+print(CURR_DIR)
+
+opts = Options()
+def vote(User):      
+    print('Operating in Windows Mode')        
+    path=CURR_DIR + '\geckodriver.exe'    
+    browser = Firefox(executable_path = path , options=opts)
+    browser.install_addon(CURR_DIR + '\uBlock0@raymondhill.net.xpi', temporary=True)    
+    print('Inserting uBlock')
+    #browser.find_element_by_class_name('sc-ifAKCX dvvOSu').click()
+    #time.sleep(5)
+    #browser.find_element_by_class_name('btn btn-primary btn-block').click()
+    print('Voting for ' + User + '!');
+    browser.get(server);
+    #element_present = EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div/button[2]'))
+    #WebDriverWait(browser, 10000).until(element_present)
+    time.sleep(5);
+    if browser.find_elements_by_xpath('/html/body/div[1]/div/div/div/div[2]/div/button[2]'):
+        browser.find_element_by_xpath('/html/body/div[1]/div/div/div/div[2]/div/button[2]').click()
+        print('Accepted Cookies');
+
+    browser.execute_script('document.body.style.MozTransform = "scale(0.5)";')
+    print('Scaling to 0.5');
+    time.sleep(10);
+
+    I=browser.find_element_by_xpath('//*[@id="playername"]')
+    print('Scrolling into View');
+    browser.execute_script("arguments[0].scrollIntoView(true);", I)
+    time.sleep(10)
+    
+    player=browser.find_element_by_xpath('//*[@id="playername"]')
+    player.send_keys(User);
+    print('Inserting Username')
+    
+    I=browser.find_element_by_xpath('//*[@id="captcha"]')
+    print('Scrolling into View');
+    browser.execute_script("arguments[0].scrollIntoView(true);", I)
+    time.sleep(10);
+
+    browser.find_element_by_xpath('//*[@id="captcha"]').click();
+    print('Voting');
+    time.sleep(10);
+    c = browser.current_url
+    if c == 'https://minecraft-server.eu/vote/fail/208F7':
+        print('Already Voted/Failed to vote');
+    else:
+        print('Sucessfully voted');
+    time.sleep(5);
+    browser.close();
+if headless == '1':        
+    opts.set_headless()
+    assert opts.headless  # Operating in headless mode 
+vote(user);
